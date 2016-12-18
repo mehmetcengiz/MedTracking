ee90cc461f03e60b8727b308ff93065fe661509e
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-12-18 21:37:54 +0300

    Working Read Data, Storage Proc, Trigger.

--- MedTrackingGui/Program.cs
+++ MedTrackingGui/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +16,58 @@ namespace MedTrackingGui
         [STAThread]
         static void Main()
         {
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source=DESKTOP-QAE100K;Initial Catalog=MedTrackingDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM Medicine";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            // Veri alma
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+            while (reader.Read()) {
+                var record = (IDataRecord) reader;
+                Console.WriteLine(@"#{0} Name: '{1}' Price: '{2}'", record[0], record[2], record[3]);
+            }
+
+            sqlConnection1.Close();
+
+            // Stored Proc. çalıştırma
+            cmd.CommandText = "LoginEmployee";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = "John";
+            cmd.Parameters.Add("@surname", SqlDbType.VarChar).Value = "Doe";
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+            while (reader.Read())
+            {
+                var record = (IDataRecord)reader;
+                Console.WriteLine(@"#{0} Name: '{1}' Surname: '{2}'", record[0], record[2], record[3]);
+            }
+
+            sqlConnection1.Close();
+
+            // MedicineGroup ekleme ve trigger çalıştırma
+            cmd.CommandText = "INSERT INTO MedicineGroups (Name) VALUES (@name)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = "YENİ GRUP2";
+
+            sqlConnection1.Open();
+
+            // Trigger should be fired
+            cmd.ExecuteNonQuery();
+
+            // ----------------------------------------------------------------------
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
