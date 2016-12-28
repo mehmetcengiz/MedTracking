24513fb5effff169e37118a2d67da1c90fb05394
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2016-12-28 06:29:19 +0300

    Minor fixes on prescription dialog

--- MedTrackingGui/ShowPrescriptionInfo.Designer.cs
+++ MedTrackingGui/ShowPrescriptionInfo.Designer.cs
@@ -23,7 +23,7 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
             "100",
             "Parol",
             "15",
@@ -38,12 +38,14 @@
 			this.tbDoctorFullName = new System.Windows.Forms.TextBox();
 			this.tbDateAndTime = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// btnOk
 			// 
 			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnOk.Location = new System.Drawing.Point(425, 338);
+			this.btnOk.Location = new System.Drawing.Point(425, 364);
 			this.btnOk.Name = "btnOk";
 			this.btnOk.Size = new System.Drawing.Size(75, 23);
 			this.btnOk.TabIndex = 19;
@@ -58,10 +60,10 @@
             this.columnHeader2});
 			this.listViewMedicines.FullRowSelect = true;
 			this.listViewMedicines.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem3});
-			this.listViewMedicines.Location = new System.Drawing.Point(111, 90);
+            listViewItem1});
+			this.listViewMedicines.Location = new System.Drawing.Point(124, 116);
 			this.listViewMedicines.Name = "listViewMedicines";
-			this.listViewMedicines.Size = new System.Drawing.Size(389, 242);
+			this.listViewMedicines.Size = new System.Drawing.Size(376, 242);
 			this.listViewMedicines.TabIndex = 16;
 			this.listViewMedicines.UseCompatibleStateImageBehavior = false;
 			this.listViewMedicines.View = System.Windows.Forms.View.Details;
@@ -69,17 +71,17 @@
 			// columnHeader1
 			// 
 			this.columnHeader1.Text = "#";
-			this.columnHeader1.Width = 59;
+			this.columnHeader1.Width = 46;
 			// 
 			// columnHeader2
 			// 
 			this.columnHeader2.Text = "Name";
-			this.columnHeader2.Width = 325;
+			this.columnHeader2.Width = 292;
 			// 
 			// lblDoctorFullName
 			// 
 			this.lblDoctorFullName.AutoSize = true;
-			this.lblDoctorFullName.Location = new System.Drawing.Point(13, 41);
+			this.lblDoctorFullName.Location = new System.Drawing.Point(26, 67);
 			this.lblDoctorFullName.Name = "lblDoctorFullName";
 			this.lblDoctorFullName.Size = new System.Drawing.Size(92, 13);
 			this.lblDoctorFullName.TabIndex = 9;
@@ -88,7 +90,7 @@
 			// lblPatientFullName
 			// 
 			this.lblPatientFullName.AutoSize = true;
-			this.lblPatientFullName.Location = new System.Drawing.Point(12, 16);
+			this.lblPatientFullName.Location = new System.Drawing.Point(25, 41);
 			this.lblPatientFullName.Name = "lblPatientFullName";
 			this.lblPatientFullName.Size = new System.Drawing.Size(93, 13);
 			this.lblPatientFullName.TabIndex = 10;
@@ -96,50 +98,69 @@
 			// 
 			// tbPatientFullName
 			// 
-			this.tbPatientFullName.Location = new System.Drawing.Point(111, 12);
+			this.tbPatientFullName.Location = new System.Drawing.Point(124, 38);
 			this.tbPatientFullName.Name = "tbPatientFullName";
 			this.tbPatientFullName.ReadOnly = true;
-			this.tbPatientFullName.Size = new System.Drawing.Size(389, 20);
+			this.tbPatientFullName.Size = new System.Drawing.Size(376, 20);
 			this.tbPatientFullName.TabIndex = 20;
 			// 
 			// tbDoctorFullName
 			// 
-			this.tbDoctorFullName.Location = new System.Drawing.Point(111, 38);
+			this.tbDoctorFullName.Location = new System.Drawing.Point(124, 64);
 			this.tbDoctorFullName.Name = "tbDoctorFullName";
 			this.tbDoctorFullName.ReadOnly = true;
-			this.tbDoctorFullName.Size = new System.Drawing.Size(389, 20);
+			this.tbDoctorFullName.Size = new System.Drawing.Size(376, 20);
 			this.tbDoctorFullName.TabIndex = 20;
 			// 
 			// tbDateAndTime
 			// 
-			this.tbDateAndTime.Location = new System.Drawing.Point(111, 64);
+			this.tbDateAndTime.Location = new System.Drawing.Point(124, 90);
 			this.tbDateAndTime.Name = "tbDateAndTime";
 			this.tbDateAndTime.ReadOnly = true;
-			this.tbDateAndTime.Size = new System.Drawing.Size(389, 20);
+			this.tbDateAndTime.Size = new System.Drawing.Size(376, 20);
 			this.tbDateAndTime.TabIndex = 20;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(25, 67);
+			this.label1.Location = new System.Drawing.Point(38, 93);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(80, 13);
 			this.label1.TabIndex = 9;
 			this.label1.Text = "Date and Time:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(124, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(376, 20);
+			this.textBox1.TabIndex = 20;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(106, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Employee Full Name:";
 			// 
 			// ShowPrescriptionInfo
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnOk;
-			this.ClientSize = new System.Drawing.Size(512, 373);
+			this.ClientSize = new System.Drawing.Size(512, 399);
 			this.Controls.Add(this.btnOk);
 			this.Controls.Add(this.listViewMedicines);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.lblDoctorFullName);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.lblPatientFullName);
 			this.Controls.Add(this.tbDateAndTime);
 			this.Controls.Add(this.tbDoctorFullName);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.tbPatientFullName);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
@@ -166,5 +187,7 @@
 		private System.Windows.Forms.TextBox tbDoctorFullName;
 		private System.Windows.Forms.TextBox tbDateAndTime;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
 	}
 }
